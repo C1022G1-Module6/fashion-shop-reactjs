44de20447faa917897f4e5578bbbcbed0f78c295
anhquan1301 <nlaquan1301@gmail.com>
2023-04-25 10:12:21 +0700

    update project fashion-shop

---
+++ src/store.js
@@ -0,0 +1,16 @@
+
+
+import { applyMiddleware, createStore } from 'redux';
+
+import { rootReducer } from './reducer';
+import  thunk from 'redux-thunk';
+
+const initialState = {}
+
+const middleware = [thunk];
+
+export const store= createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(...middleware)
+)
